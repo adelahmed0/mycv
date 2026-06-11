e5115cc746d5d41dae565a79266173aa1dd77f8e
Adel Ahmed <adel15952@gmail.com>
2026-06-11 19:59:19 +0300

    feat: implement AuthService for user authentication with signup functionality

---
+++ src/users/auth.service.ts
@@ -0,0 +1,21 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { UsersService } from './users.service';
+
+@Injectable()
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async signup(email: string, password: string) {
+    // see if email is in use
+    const users = await this.usersService.find(email);
+    if (users.length) {
+      throw new BadRequestException('email in use');
+    }
+
+    // hash the user's password
+    // create a new user and save it
+    // return the user
+  }
+
+  signin() {}
+}
