2503776610d69e600ffda77ea8bf18dd343e2fc0
Adel Ahmed <adel15952@gmail.com>
2026-06-17 00:17:50 +0300

    feat: create AuthController for user authentication and update API routes for signup/signin

---
+++ src/users/auth.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { AuthService } from './auth.service';
+import { Serialize } from '../interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
+
+@Controller('auth')
+@Serialize(UserDto)
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('sign-up')
+  signUp(@Body() body: CreateUserDto) {
+    return this.authService.signup(body.email, body.password);
+  }
+
+  @Post('signin')
+  signIn(@Body() body: CreateUserDto) {
+    return this.authService.signin(body.email, body.password);
+  }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,23 +6,17 @@ import {
   Query,
   Delete,
   Patch,
-  Post,
   ParseIntPipe,
 } from '@nestjs/common';
-import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
-import { AuthService } from './auth.service';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 
 @Controller('users')
 @Serialize(UserDto)
 export class UsersController {
-  constructor(
-    private usersService: UsersService,
-    private authService: AuthService,
-  ) {}
+  constructor(private usersService: UsersService) {}
 
   @Get(':id')
   findUser(@Param('id', ParseIntPipe) id: number) {
@@ -45,15 +39,5 @@ export class UsersController {
     @Body() body: UpdateUserDto,
   ) {
     return this.usersService.update(id, body);
-  }
-
-  @Post('sign-up')
-  signUp(@Body() body: CreateUserDto) {
-    return this.authService.signup(body.email, body.password);
-  }
-
-  @Post('signin')
-  signIn(@Body() body: CreateUserDto) {
-    return this.authService.signin(body.email, body.password);
   }
 }
