0a5e2a4aa39b7a2ff4b3d3d4419c5f084766ce3d
Adel Ahmed <adel15952@gmail.com>
2026-06-16 22:56:29 +0300

    feat: integrate AuthService into UsersController for user signup process

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,6 +1,9 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
 
+const scrypt = promisify(_scrypt);
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
@@ -13,8 +16,16 @@ export class AuthService {
     }
 
     // hash the user's password
+    // generate a salt
+    const salt = randomBytes(8).toString('hex');
+    // hash the salt and the password together
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+    // join the hash and the salt
+    const result = salt + '.' + hash.toString('hex');
     // create a new user and save it
+    const user = await this.usersService.create(email, result);
     // return the user
+    return user;
   }
 
   signin() {}
