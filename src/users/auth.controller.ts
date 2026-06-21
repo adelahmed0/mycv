e259f6d1aa281358706b5b0ac00990aeb9a63a2f
Adel Ahmed <adel15952@gmail.com>
2026-06-22 00:18:41 +0300

    feat: add color management endpoints to AuthController for session handling

--- src/users/auth.controller.ts
+++ src/users/auth.controller.ts
@@ -1,14 +1,27 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Session, Get, Param } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { AuthService } from './auth.service';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
+
+interface SessionData {
+  color?: string;
+}
 
 @Controller('auth')
 @Serialize(UserDto)
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @Get('/colors/:color')
+  setColor(@Param('color') color: string, @Session() session: SessionData) {
+    session.color = color;
+  }
+
+  @Get('/colors')
+  getColor(@Session() session: SessionData): string | undefined {
+    return session.color;
+  }
   @Post('sign-up')
   signUp(@Body() body: CreateUserDto) {
     return this.authService.signup(body.email, body.password);
