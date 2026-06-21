7b06e6d5f656f8d0bb53b1d0062fa413270984e6
Adel Ahmed <adel15952@gmail.com>
2026-06-21 23:40:43 +0300

    feat: add cookie-session middleware for session management in the application

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
+import cookieSession from 'cookie-session';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(
+    cookieSession({
+      keys: ['asdfasdf'],
+    }),
+  );
 
   app.useGlobalPipes(
     new ValidationPipe({
