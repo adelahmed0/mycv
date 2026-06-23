c7e27e6af43b3f9ee774e22733842949bdbaf6e0
Adel Ahmed <adel15952@gmail.com>
2026-06-24 00:37:36 +0300

    feat: integrate CurrentUserInterceptor into AuthController and UsersModule for enhanced user context management

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,10 +5,11 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { AuthService } from './auth.service';
 import { User } from './users.entity';
+import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [AuthController, UsersController],
-  providers: [UsersService, AuthService],
+  providers: [UsersService, AuthService, CurrentUserInterceptor],
 })
 export class UsersModule {}
