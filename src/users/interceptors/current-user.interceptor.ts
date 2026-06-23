6a588785a8321958b76069f88efe0ca16d665d15
Adel Ahmed <adel15952@gmail.com>
2026-06-24 00:29:42 +0300

    feat: add CurrentUserInterceptor to retrieve and attach the current user to the request context

---
+++ src/users/interceptors/current-user.interceptor.ts
@@ -0,0 +1,41 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { Observable } from 'rxjs';
+import { User } from '../users.entity';
+import { UsersService } from '../users.service';
+
+interface SessionData {
+  userId?: number;
+}
+
+interface RequestWithUser extends Request {
+  session: SessionData;
+  currentUser?: User;
+}
+
+@Injectable()
+export class CurrentUserInterceptor implements NestInterceptor {
+  constructor(private usersService: UsersService) {}
+
+  async intercept(
+    context: ExecutionContext,
+    handler: CallHandler,
+  ): Promise<Observable<unknown>> {
+    const request = context.switchToHttp().getRequest<RequestWithUser>();
+    const { userId } = request.session || {};
+
+    if (userId) {
+      const user = await this.usersService.findOne(userId);
+      if (user) {
+        request.currentUser = user;
+      }
+    }
+
+    return handler.handle();
+  }
+}
