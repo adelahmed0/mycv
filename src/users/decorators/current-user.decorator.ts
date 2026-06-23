d50cc0378c4bf6d529cbeccb31fa4a01c6d98b75
Adel Ahmed <adel15952@gmail.com>
2026-06-23 22:55:09 +0300

    fix: update CurrentUser decorator to correctly retrieve the current user from the request context

--- src/users/decorators/current-user.decorator.ts
+++ src/users/decorators/current-user.decorator.ts
@@ -1,7 +1,10 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { User } from '../users.entity';
 
 export const CurrentUser = createParamDecorator(
-  (data: any, context: ExecutionContext) => {
-    return 'test';
+  (data: never, context: ExecutionContext): User => {
+    const request = context.switchToHttp().getRequest<{ currentUser: User }>();
+
+    return request.currentUser;
   },
 );
