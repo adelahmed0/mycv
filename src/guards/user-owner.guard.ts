c6e8ef1af540de4d13d5f4f716f62149bf1bffbe
Adel Ahmed <adel15952@gmail.com>
2026-06-28 19:36:09 +0300

    feat: add UserOwnerGuard to restrict access based on user ownership for enhanced security

---
+++ src/guards/user-owner.guard.ts
@@ -0,0 +1,26 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { User } from '../users/users.entity';
+
+interface RequestWithUser {
+  params: { id: string };
+  currentUser?: User;
+}
+
+@Injectable()
+export class UserOwnerGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest<RequestWithUser>();
+    const paramId = parseInt(request.params.id, 10);
+
+    if (!request.currentUser || request.currentUser.id !== paramId) {
+      throw new ForbiddenException('not authorized');
+    }
+
+    return true;
+  }
+}
