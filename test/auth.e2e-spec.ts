1ca47faf68ebfbf01a31ce3c6ebabee0ab3fab4c
Adel Ahmed <adel15952@gmail.com>
2026-07-14 00:50:56 +0300

    refactor: clean up imports and enhance user authentication tests for improved clarity and functionality

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -40,7 +40,18 @@ describe('Authentication system', () => {
       });
   });
 
-  afterEach(async () => {
-    await app.close();
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = `test1234@test.com`;
+    const res = await request(app.getHttpServer())
+      .post('/auth/sign-up')
+      .send({ email, password: '123456' })
+      .expect(201);
+    const cookie = res.get('Set-Cookie');
+    const { body } = await request(app.getHttpServer());
+    await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+    expect(body.email).toEqual(email);
   });
 });
