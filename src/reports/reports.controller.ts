0ba1936d5f5f98fcd130eff39c90aee50fc50869
Adel Ahmed <adel15952@gmail.com>
2026-07-14 23:37:33 +0300

    feat: implement CreateReportDto with validation and update ReportsController to use ReportsService

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,7 +1,11 @@
 import { Controller, Post, Body } from '@nestjs/common';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { ReportsService } from './reports.service';
 
 @Controller('reports')
 export class ReportsController {
+  constructor(private reportsService: ReportsService) {}
+
   @Post()
   createReport(@Body() body: CreateReportDto) {
     return 'report created';
