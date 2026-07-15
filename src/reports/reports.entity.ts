79fb2c4087b19186675593edbab9844ac54a25cb
Adel Ahmed <adel15952@gmail.com>
2026-07-15 23:25:05 +0300

    feat: establish relationship between User and Report entities by adding user reference in Report and reports collection in User

--- src/reports/reports.entity.ts
+++ src/reports/reports.entity.ts
@@ -1,5 +1,5 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
-
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { User } from 'src/users/users.entity';
 @Entity()
 export class Report {
   @PrimaryGeneratedColumn()
@@ -25,4 +25,7 @@ export class Report {
 
   @Column()
   mileage: number;
+
+  @ManyToOne(() => User, (user) => user.reports)
+  user: User;
 }
