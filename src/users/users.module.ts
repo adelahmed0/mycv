db860e489af4c8dbbe89032de73c5535c3587b0c
Adel Ahmed <adel15952@gmail.com>
2026-07-17 01:20:39 +0300

    feat: replace CurrentUserInterceptor with CurrentUserMiddleware for user session management in UsersModule

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,25 +1,20 @@
-import { Module } from '@nestjs/common';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { AuthService } from './auth.service';
 import { User } from './users.entity';
-import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
 import { UserOwnerGuard } from '../guards/user-owner.guard';
+import { CurrentUserMiddleware } from './middlewares/current-user.middleware';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [AuthController, UsersController],
-  providers: [
-    UsersService,
-    AuthService,
-    UserOwnerGuard,
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: CurrentUserInterceptor,
-    },
-  ],
+  providers: [UsersService, AuthService, UserOwnerGuard],
 })
-export class UsersModule {}
+export class UsersModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(CurrentUserMiddleware).forRoutes('*');
+  }
+}
